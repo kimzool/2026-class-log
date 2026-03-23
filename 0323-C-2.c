d0868d760a74cea52b588edcb79b146025bbb6a7
kimzool <lusiu9757@gmail.com>
2026-03-23 15:50:15 +0900

    for문을 이용한 구구단 정렬

---
+++ 0323-C-2.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+    int a, b,c = 0;
+
+
+    for (a = 1; a < 13; a+=3) {
+        
+        c = a + 1;
+
+        printf("\n   %d단", a);
+        printf("\t\t\t   %d단",c);
+        printf("\t\t\t   %d단", c = c + 1);
+        
+        for (b = 1;b <= 9;++b) {
+
+            printf("\n%d * %d = %2d\t", a, b, a * b);
+            printf("\t%d * %d = %2d\t", a+1 , b, (a + 1) * b);
+            printf("\t%d * %d = %2d\t", a + 2, b, (a + 2) * b);
+
+
+        }
+
+
+
+    }
+
+    return 0;
+}
